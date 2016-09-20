1c5ac8dbbeea13a73c96e0a277be92cf60f5850a
Eduardo Luz <luz.eduardo@gmail.com>
2016-09-20 08:55:01 -0300

    structuring template

--- src/client/components/Main.js
+++ src/client/components/Main.js
@@ -5,7 +5,7 @@ import MovieSearch from './MovieSearch'
 class Main extends React.Component {
   render(){
     return (
-      <div className="columns is-mobile">
+      <div className="columns">
         <div className="column">
 
           <MovieSearch />
