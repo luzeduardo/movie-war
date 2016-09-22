7d8436e420ebbe3d4a3c1011e931c9da7e166bcf
Eduardo Luz <luz.eduardo@gmail.com>
2016-09-22 19:09:13 -0300

    structure change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import movieApp from './reducers/index'
-// import {createStore} from 'redux';
+import {createStore} from 'redux';
 
 import App from './App';
 import './index.css';
