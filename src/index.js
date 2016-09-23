f9d06521e2ac79f959abfc71a0dd9c43c5ad0e5f
Eduardo Luz <luz.eduardo@gmail.com>
2016-09-23 07:23:54 -0300

    connect store to app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import movieApp from './reducers/index'
-import {createStore} from 'redux';
 
 import App from './App';
 import './index.css';
--- src/store.js
+++ src/store.js
@@ -1,17 +1,3 @@
 import {createStore} from 'redux'
-import movieAdd from './reducers/movie'
-import {addMovie, setVisibilityFilter, VisibilityFilters} from './actions/'
-
+import movieAdd from './reducers'
 let store = createStore(movieAdd)
-
-console.log(store.getState());
-
-let unsubscribe = store.subscribe(() => {
-  console.log(store.getState())
-})
-
-store.dispatch(addMovie('Mommy'))
-store.dispatch(addMovie('Mummy'))
-store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_ALL))
-
-unsubscribe()
