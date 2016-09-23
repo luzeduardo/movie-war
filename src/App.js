ce4b79ea1e581019808902cd9ac1ae6760c36326
Eduardo Luz <luz.eduardo@gmail.com>
2016-09-23 07:34:17 -0300

    ES6 notation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import 'bulma/css/bulma.css'
 import './App.css';
 import Main from './components/Main'
 
-class App extends Component {
-  render() {
-    return (
-      <Main/>
-    );
-  }
-}
+const App = () => (<Main/>)
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,15 +2,11 @@ import React from 'react';
 import MovieList from './MovieList'
 import MovieSearch from './MovieSearch'
 
-class Main extends React.Component {
-  render(){
-    return (
-      <div className="container is-fluid">
-          <MovieSearch />
-          <MovieList />
-      </div>
-  )
-  }
-}
+const Main = () => (
+  <div className="container is-fluid">
+      <MovieSearch />
+      <MovieList />
+  </div>
+)
 
 export default Main;
