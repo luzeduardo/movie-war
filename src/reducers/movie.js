67629fa045bd25a5834141905a66698e8e9ca2d5
Eduardo Luz <luz.eduardo@gmail.com>
2016-09-26 08:21:58 -0300

    state changes test

--- src/reducers/movie.js
+++ src/reducers/movie.js
@@ -1,5 +1,15 @@
 function movieAdd(state = [], action){
+  switch (action.type) {
+    case 'ADD_MOVIE':
+      return [...state, {
+        statechangedprop: true,
+        data: action.data
+      }]
+      break;
+    default:
+      return state;
+
+  }
   console.log(state);
   console.log(action);
-  return state;
 }
