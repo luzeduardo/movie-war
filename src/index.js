b2b86389f0711263a5a9eef734c70738c7dfe80c
Eduardo Luz <luz.eduardo@gmail.com>
2016-09-28 07:38:09 -0300

    router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {Router, Route, browserHistory} from 'react-router'
+
 import App from './App';
+import MovieSearch from './components/MovieSearch'
 import './index.css';
 
 import {Provider} from 'react-redux'
@@ -9,7 +12,12 @@ import store from './store'
 
 ReactDOM.render(
   <Provider store={store}>
-    <App/>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <Route path="/search" component={MovieSearch} />
+      </Route>
+
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
