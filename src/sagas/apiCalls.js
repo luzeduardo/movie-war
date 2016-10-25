d83d963e74bdfb4e5a56ae4a1eda12b045342d2f
Eduardo Luz <luz.eduardo@gmail.com>
2016-10-25 12:33:57 -0200

    studying redux-saga

--- src/sagas/apiCalls.js
+++ src/sagas/apiCalls.js
@@ -2,5 +2,5 @@ import MovieServiceApi from '../api/movieServiceApi';
 
 export const loadMovie = () => {
   console.log('loading movie');
-  return TravelServiceApi.returnMovie().then(res => res);
+  return MovieServiceApi.returnMovie().then(res => res);
 };
