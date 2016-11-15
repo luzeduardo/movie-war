8ff4d9d21cfb95b29df347dd66773724a665a2b9
Eduardo Luz <luz.eduardo@gmail.com>
2016-11-15 18:37:21 -0200

    env

--- src/api/movieApi.js
+++ src/api/movieApi.js
@@ -1,6 +1,8 @@
 import runtimeEnv from '@mars/heroku-js-runtime-env';
+const env = runtimeEnv();
 
 class MovieApi {
+
  static returnMovie(search) {
      return fetch(`${env.MOVIE_API_ADDRESS}?s=${search.search}`)
      .then(
